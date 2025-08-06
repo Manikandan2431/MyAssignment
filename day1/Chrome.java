cb45eb02a34ad9c6d8cf60d2e152d694d8c88e65
Manikandan2431 <mani.mca.ssn@gmail.com>
2025-08-06 21:46:28 +0530

    Add files via upload
    Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int release = 2008;
+		char shortcutKey ='C';
+		System.out.println("version:" +version+"\ndeveloper:"+developer+"\nisBeta:"+isBeta+
+				"\nrelease:"+release+"\nshortcutKey:"+shortcutKey);
+
+	}
+
+}
